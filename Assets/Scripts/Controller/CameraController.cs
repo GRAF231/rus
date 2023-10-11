7581eed74999d327e7403e9493ccc979f96ee25e
Nerol-Windwaker <64909760+Nerol-Windwaker@users.noreply.github.com>
2023-10-11 15:58:43 +0300

    Разные области видимости камеры в десктопе и телефоне

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class CameraController : MonoBehaviour
 {
     GameObject _player = null;
-
+    PixelPerfectCamera camera;
     Vector3 _delta = new Vector3(0, 0, -10f);
 
     public void SetPlayer(GameObject player) { _player = player; }
+
+    public void Start()
+    {
+        UnityEngine.Debug.Log("Her1");
+
+        camera = gameObject.GetComponent<PixelPerfectCamera>();
+        if (Util.isMobile())
+        {
+            camera.assetsPPU = 45;
+        }
+        else
+        {
+            camera.assetsPPU = 25;
+        }
+    }
+
+
+
     void Update()
     {
         if (object.ReferenceEquals(_player, null))
